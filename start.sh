dd6c52e451b4b9858d16da0578a26b8a423a8ddf
Alex <137611210+alex1028199@users.noreply.github.com>
2024-02-28 18:47:42 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# Function to display message with delay
-display_message() {
-    echo "$1"
-    sleep 1
-}
+plugins_dir="/mnt/server/plugins"
+mkdir -p "$plugins_dir"
+cd "$plugins_dir"
 
-# Check if plugins folder exists, if not, create it
-if [ ! -d "/home/container/plugins" ]; then
-    display_message "Creating plugins folder..."
-    mkdir /home/container/plugins
-    display_message "Plugins folder created."
+# Check if plugins folder exists and create it if it doesn't
+if [ ! -d "$plugins_dir" ]; then
+    echo "Creating plugins folder..."
+    mkdir -p "$plugins_dir"
 fi
 
-# Check if HibernateX.jar already exists in plugins folder
-if [ ! -f "/home/container/plugins/HibernateX.jar" ]; then
-    display_message "Downloading HibernateX.jar..."
-    wget -q -O /home/container/plugins/HibernateX.jar https://github.com/alex1028199/artixegg/releases/download/Hiber/HibernateX.jar
-    display_message "Download complete."
+# Check if HibernateX.jar exists
+if [ ! -f HibernateX.jar ]; then
+    echo "HibernateX.jar not found, downloading..."
+    curl -s -L -o HibernateX.jar https://github.com/alex1028199/artixegg/releases/download/Hiber/HibernateX.jar > /dev/null
+    if [ -f HibernateX.jar ]; then
+        echo "Plugin downloaded successfully."
+    else
+        echo "Failed to download plugin."
+    fi
 else
-    display_message "HibernateX.jar already exists in plugins folder. Skipping download."
+    echo "HibernateX.jar already exists."
 fi
 
-# Run the server with specified parameters
-display_message "Starting the server..."
-java -Xms128M -Xmx128M -Dterminal.jline=false -Dterminal.ansi=true -jar /home/container/server.jar
+# Run Java command
+java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar "${plugins_dir}/HibernateX.jar"
