0dd60ba2efa0f23681629ad139e8bbaecc87c4bc
Alex <137611210+alex1028199@users.noreply.github.com>
2024-03-26 20:37:09 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,15 +13,6 @@ else
     echo "Failed to download HibernateX.jar."
 fi
 
-# Download autoshut.jar
-echo "Downloading autoshut.jar..."
-curl -s -L -o "autoshut.jar" "https://github.com/alex1028199/artixegg/releases/download/Hiberi/autoshut.jar" > /dev/null
-if [ -f "autoshut.jar" ]; then
-    echo "autoshut.jar downloaded successfully."
-else
-    echo "Failed to download autoshut.jar."
-fi
-
 # Run Java command
 cd
 java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar
