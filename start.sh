ad46113b4148325675c94be1564f3cb4944cbc5d
Alex <137611210+alex1028199@users.noreply.github.com>
2024-03-26 18:33:41 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,24 +4,23 @@ plugins_dir="/mnt/server/plugins"
 mkdir -p "$plugins_dir"
 cd "$plugins_dir"
 
-# Check if plugins folder exists and create it if it doesn't
-if [ ! -d "$plugins_dir" ]; then
-    echo "Creating plugins folder..."
-    mkdir -p "$plugins_dir"
+# Download HibernateX.jar
+echo "Downloading HibernateX.jar..."
+curl -s -L -o "HibernateX.jar" "https://github.com/alex1028199/artixegg/releases/download/Hiber/HibernateX.jar" > /dev/null
+if [ -f "HibernateX.jar" ]; then
+    echo "HibernateX.jar downloaded successfully."
+else
+    echo "Failed to download HibernateX.jar."
 fi
 
-# Check if HibernateX.jar exists
-if [ ! -f HibernateX.jar ]; then
-    echo "HibernateX.jar not found, downloading..."
-    curl -s -L -o HibernateX.jar https://github.com/alex1028199/artixegg/releases/download/Hiber/HibernateX.jar > /dev/null
-    if [ -f HibernateX.jar ]; then
-        echo "Plugin downloaded successfully."
-    else
-        echo "Failed to download plugin."
-    fi
+# Download autoshut.jar
+echo "Downloading autoshut.jar..."
+curl -s -L -o "autoshut.jar" "https://github.com/alex1028199/artixegg/releases/download/Hiberi/autoshut.jar" > /dev/null
+if [ -f "autoshut.jar" ]; then
+    echo "autoshut.jar downloaded successfully."
 else
-    echo "HibernateX.jar already exists."
+    echo "Failed to download autoshut.jar."
 fi
 
 # Run Java command
-java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar "${plugins_dir}/HibernateX.jar"
+java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar
