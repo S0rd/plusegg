36264e0b91c0f00001ff183319e28b929f7b81da
Alex <137611210+alex1028199@users.noreply.github.com>
2024-03-31 10:32:39 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,7 +12,24 @@ if [ -f "HibernateX.jar" ]; then
 else
     echo "Failed to download HibernateX.jar."
 fi
+
+# Download Clearlagg.jar
+curl -s -L -o "Clearlagg.jar" "https://github.com/alex1028199/artixegg/releases/download/Clearlagg/Clearlagg.jar" > /dev/null
+if [ -f "Clearlagg.jar" ]; then
+    echo "Clearlagg.jar downloaded successfully."
+else
+    echo "Failed to download Clearlagg.jar."
+fi
+
+# Download Spark.jar
+curl -s -L -o "Spark.jar" "https://github.com/alex1028199/artixegg/releases/download/Spark/spark.jar" > /dev/null
+if [ -f "Spark.jar" ]; then
+    echo "Spark.jar downloaded successfully."
+else
+    echo "Failed to download Spark.jar."
+fi
+
 echo "THIS SYSTEM IS RUNNING BY SPARIXXNODES!"
 # Run Java command
-cd
+cd ..
 java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar
