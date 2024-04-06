30c2cd9862e6d87e90c8ffc01cb2f66297f272a0
Alex <137611210+alex1028199@users.noreply.github.com>
2024-04-06 20:24:43 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,14 +13,6 @@ else
     echo "Failed to download HibernateX.jar."
 fi
 
-# Download Clearlagg.jar
-curl -s -L -o "Clearlagg.jar" "https://github.com/alex1028199/artixegg/releases/download/Clearlagg/Clearlagg.jar" > /dev/null
-if [ -f "Clearlagg.jar" ]; then
-    echo "Clearlagg.jar downloaded successfully."
-else
-    echo "Failed to download Clearlagg.jar."
-fi
-
 # Download Spark.jar
 curl -s -L -o "Spark.jar" "https://github.com/alex1028199/artixegg/releases/download/Spark/spark.jar" > /dev/null
 if [ -f "Spark.jar" ]; then
