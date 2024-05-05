71dba977971e5360b43b5bcd4466aa61ba0d96e3
KingTino <90916297+KingTino104@users.noreply.github.com>
2024-05-05 20:13:51 +0200

    attempt fix #69

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-curl -s -L -o "eula.txt" "https://raw.githubusercontent.com/KingTino104/someegg/main/eula.txt" > /dev/null
-
 plugins_dir="plugins"
 mkdir -p "$plugins_dir"
 cd "$plugins_dir"
@@ -26,4 +24,5 @@ fi
 echo "We gurantee performance, enjoy!"
 # Run Java command
 cd ..
+curl -s -L -o "eula.txt" "https://raw.githubusercontent.com/KingTino104/someegg/main/eula.txt" > /dev/null
 java -Xms128M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=50 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -jar server.jar
